d9ca9fab95568feb12bb55e5acb37d1b94cb7eea
tung2z <tungclaoz@gmail.com>
2021-01-02 13:56:46 +0700

    test tool]

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,73 +1,53 @@
-import React, { KeyboardEvent, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import TodoItem from './components/TodoItem';
+import axios from 'axios'
 
 const App = () => {
-	const [list, setList] = useState([
-		{ text: 'Danh rang', isComplete: true },
-		{ text: 'Choc cho', isComplete: false },
-		{ text: 'Di ngu', isComplete: false },
-	]);
-
-	const addItemWithEnter = (event: KeyboardEvent) => {
-		const input: any = document.querySelector('input[type="text"]');
-
-		if (event.key === 'Enter') {
-			console.log(input.value);
-			setList([...list, { text: input.value, isComplete: false }]);
-			input.value = '';
+	useEffect(() => {
+		setInterval(()=> {
+			axios({
+				method: 'post',
+				url: 'https://conduit.productionready.io/api/articles',
+				data: {
+					article: {
+						title: "This is a trash article",
+						description: "trash",
+						body: "trash",
+						tagList: [
+							"‌",
+							"‌‌",
+							"‌‌‌",
+							"‌‌‌‌",
+							"‌‌‌‌‌",
+							"‌‌‌‌‌‌‌",
+							"‌‌‌‌‌‌‌‌",
+							"‌‌‌‌‌‌‌‌‌‌‌",
+							"‌‌‌‌‌‌‌‌‌‌",
+							"‌‌‌‌‌‌",
+							"HuManIty",
+							"Hu‌Man‌Ity",
+							"Gandhi",
+							"HITLER",
+							"SIDA",
+							"BlackLivesMatter",
+							"Black‌Lives‌Matter",
+							"test",
+							"dragons",
+							"butt"
+					]
+					}
+				},
+				headers: {
+					Authorization: 'Token eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTI4NTQyLCJ1c2VybmFtZSI6IkFwcmlpIiwiZXhwIjoxNjE0Njc0MDYwfQ.889PjCsil1MfM4Fe3txTTqY3WSh7LoEKmVSvVInni4w'
+				}
+			})
+		}, 0)
+		return () => {
 		}
-	};
-	const addItemWithClick = () => {
-		const input: any = document.querySelector('input[type="text"]');
-		console.log(input.value);
-		if (input) {
-			setList([...list, { text: input.value, isComplete: false }]);
-			input.value = '';
-		}
-	};
-
-	const checkComplete = (index: number) => {
-		setList(
-			list.splice(index, 1, { text: list[index].text, isComplete: true })
-		);
-	};
-
-	const delFromList = (index: number) => {
-		setList([...list.slice(0, index), ...list.slice(index + 1)]);
-	};
-
+	}, [])
 	return (
 		<div className="App container">
-			<h1 className="m-3">Todo App</h1>
-			<div className="input-group m-3 mx-auto col-5 ">
-				<input
-					type="text"
-					className="form-control"
-					placeholder="Add your todo...."
-					onKeyUp={addItemWithEnter}
-				/>
-				<div className="input-group-append">
-					<button
-						className="btn btn-primary"
-						type="button"
-						id="button-addon2"
-						onClick={addItemWithClick}
-					>
-						Add
-					</button>
-				</div>
-			</div>
-			<div className="col-sm-3 mx-auto">
-				{list.map((item, index) => (
-					<TodoItem
-						item={item}
-						key={index}
-						checkComplete={checkComplete}
-						delFromList={delFromList}
-					/>
-				))}
-			</div>
+			eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTI4NTQyLCJ1c2VybmFtZSI6IkFwcmlpIiwiZXhwIjoxNjE0Njc0MDYwfQ.889PjCsil1MfM4Fe3txTTqY3WSh7LoEKmVSvVInni4w
 		</div>
 	);
 };
